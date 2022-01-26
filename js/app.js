bb749bbe2f72777e14f2d32c0ef7d2424544e33e
kamalus <formation.kamal.haddou@gmail.com>
2022-01-26 15:56:42 +0100

    VersiON finie du ShiFuMi IA V1

--- js/app.js
+++ js/app.js
@@ -43,7 +43,7 @@ let reset = document.querySelector('#reset')
 
 /**   DEBUT DRAG AND DROP*/
 /***********************drag */
-const dragPierreJ1 = (event) => {
+const dragJ1 = (event) => {
 
     // console.log(event)
     // console.log(event.target.attributes[0].textContent)
@@ -74,9 +74,9 @@ const dragPierreJ1 = (event) => {
 /*******************************drop */
 
 
-const dropPierreJ1 = (event) => {
+const dropJ1 = (event) => {
 
-
+    // console.log(event)
     if (event.target.attributes[0].textContent === 'choixjoueur') {
 
         if (dragPierre = true && dragCiseau === false && dragPapier === false) {
@@ -92,8 +92,8 @@ const dropPierreJ1 = (event) => {
 
 }
 
-body.ondragleave = dropPierreJ1
-body.ondrag = dragPierreJ1
+body.ondragleave = dropJ1
+body.ondrag = dragJ1
 
 /**************************RESET */
 
@@ -114,12 +114,50 @@ reset.onclick = resetShifumi
 /**   FIN DRAG AND DROP ET DU RESET */
 
 /***********************DEBUT DU PROGRAMME DE LANCEMENT */
+
+
+
+
+
+// const iaVersion1 = () => {
+
+//     imageChoisitIa.innerHTML = `<img src="img/interogation.svg">`
+
+//     choixJoueurOften = TableauChoixJoueur[Math.floor(Math.random()*TableauChoixJoueur.length)]
+//     console.log("Le joueur joue le plus souvent "+choixJoueurOften)
+
+
+
+//     setTimeout(() => {
+//         if (choixIaV1 === 0) {
+//             imageChoisitIa.innerHTML = `<img src="img/pierre.svg">`
+//         }
+//         if (choixIa === 1) {
+//             imageChoisitIa.innerHTML = `<img src="img/papier.svg">`
+//         }
+//         if (choixIa === 2) {
+//             imageChoisitIa.innerHTML = `<img src="img/ciseau.svg">`
+//         }
+//     }, 500)
+// }
+
+
+
+let TableauChoixJoueur = [0,1,2]
+let choixJoueurOften = ""
 let choixIa = ''
 
 const iaZero = () => {
 
     imageChoisitIa.innerHTML = `<img src="img/interogation.svg">`
-    choixIa = Math.floor(Math.random() * 3)
+
+    choixJoueurOften = TableauChoixJoueur[Math.floor(Math.random()*TableauChoixJoueur.length)]
+    console.log("Le joueur joue le plus souvent  "+choixJoueurOften)
+    if (choixJoueurOften === 0){choixIa = 1}
+    if (choixJoueurOften === 1){choixIa = 2}
+    if (choixJoueurOften === 2){choixIa = 0}
+
+    
     setTimeout(() => {
         if (choixIa === 0) {
             imageChoisitIa.innerHTML = `<img src="img/pierre.svg">`
@@ -131,9 +169,12 @@ const iaZero = () => {
             imageChoisitIa.innerHTML = `<img src="img/ciseau.svg">`
         }
     }, 500)
+
+
 }
 
 const affichageResultat = () => {
+    /***!!ATTENTION PENSER A MODIFIER DANS LE IF LA VARIABLE SUIVANT LA VERSION DE */
     /*******VICTOIRE */
 
     if (dragCiseau === true && choixIa === 1) {
@@ -141,45 +182,57 @@ const affichageResultat = () => {
         scoreJ1 += 1
         divScoreJ1.innerHTML = `${scoreJ1}`
         sonVictoire.play()
+        TableauChoixJoueur.push(2)
 
-    } else if (dragPierre === true && choixIa === 2) {
+    } else if (dragPierre === true && choixIa  === 2) {
         gagne.style.display = 'block';
         scoreJ1 += 1
         divScoreJ1.innerHTML = `${scoreJ1}`
         sonVictoire.play()
+        TableauChoixJoueur.push(0)
 
-    } else if (dragPapier === true && choixIa === 0) {
+    } else if (dragPapier === true && choixIa  === 0) {
         gagne.style.display = 'block';
         scoreJ1 += 1
         divScoreJ1.innerHTML = `${scoreJ1}`
         sonVictoire.play()
+        TableauChoixJoueur.push(1)
     }
     /*****EGALITE */
 
-    if (dragCiseau === true && choixIa === 2) {
+    if (dragCiseau === true && choixIa  === 2) {
         egalite.style.display = 'block'
+        TableauChoixJoueur.push(2)
 
-    } else if (dragPierre === true && choixIa === 0) {
+    } else if (dragPierre === true && choixIa  === 0) {
         egalite.style.display = 'block'
-    } else if (dragPapier === true && choixIa === 1) {
+        TableauChoixJoueur.push(0)
+
+
+    } else if (dragPapier === true && choixIa  === 1) {
         egalite.style.display = 'block'
+        TableauChoixJoueur.push(1)
+        
     }
     /******DEFAITE */
-    if (dragCiseau === true && choixIa === 0) {
+    if (dragCiseau === true && choixIa  === 0) {
         perdu.style.display = 'block';
         scoreIa += 1
         sonDefaite.play()
         divScoreIa.innerHTML = `${scoreIa}`
-    } else if (dragPierre === true && choixIa === 1) {
+        TableauChoixJoueur.push(2)
+    } else if (dragPierre === true && choixIa  === 1) {
         perdu.style.display = 'block';
         scoreIa += 1
         sonDefaite.play()
         divScoreIa.innerHTML = `${scoreIa}`
-    } else if (dragPapier === true && choixIa === 2) {
+        TableauChoixJoueur.push(0)
+    } else if (dragPapier === true && choixIa  === 2) {
         perdu.style.display = 'block';
         scoreIa += 1
         sonDefaite.play()
         divScoreIa.innerHTML = `${scoreIa}`
+        TableauChoixJoueur.push(1)
     }
 
 }
@@ -193,3 +246,13 @@ const lancement = () => {
 }
 
 boutonPlay.onclick = lancement
+
+const consoleTesteur = ()=>{
+
+        console.log("tableau:" + TableauChoixJoueur)
+        console.log("taille du tableau"  + TableauChoixJoueur.length)
+        
+        
+        
+}
+document.onclick = consoleTesteur
